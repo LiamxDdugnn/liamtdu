80b5ce4549a9261242505c5d9693d9db7e923971
LiamxDdugnn <tdungx02@gmail.com>
2025-03-08 21:32:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
+const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
@@ -8,6 +8,6 @@ const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và nh�
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const giftImage = 'https://i.imgur.com/m3EgGC3.jpeg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
